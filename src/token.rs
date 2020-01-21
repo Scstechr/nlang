cb23a790a6cf3923e60f42dde649453dfae81df3
Scstechr <teufelkonig@gmail.com>
2020-01-21 13:10:46 +0900

    define consts in token.rs

--- src/token.rs
+++ src/token.rs
@@ -3,7 +3,32 @@
 
 type TokenType = String;
 
-struct Token{
+struct Token {
     Type: TokenType,
     Literal: String,
 }
+
+const ILLEGAL: &str = "ILLEGAL";
+const EOF: &str = "EOF";
+
+// Identifier and Literal
+const IDENT: &str = "IDENT"; // add, foobar, x, y, ...
+const INT: &str = "INT";
+
+// Operator
+const ASSIGN: &str = "=";
+const PLUS: &str = "+";
+
+// Deliminator
+const COMMA: &str = ",";
+const SEMICOLON: &str = ";";
+
+// Prances and Braces
+const LPAREN: &str = "(";
+const RPAREN: &str = ")";
+const LBRACE: &str = "{";
+const RBRACE: &str = "}";
+
+// Keywords
+const FUNCTION: &str = "FUNCTION";
+const LET: &str = "LET";

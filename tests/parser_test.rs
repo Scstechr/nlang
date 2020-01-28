77687b4481260de7e1996ba8f2612382cb7af638
Scstechr <teufelkonig@gmail.com>
2020-01-28 17:51:17 +0900

    first part of parser_test_let_statements()

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -1,12 +1,13 @@
 extern crate nlang;
-use nlang::lexer;
+use nlang::{lexer, parser};
 
 #[test]
 fn parser_test_let_statements() {
     let input = "let x = 5;
 let y = 10;
 let foobar = 838383;";
-    let mut l = lexer::new(input);
+    let l = lexer::new(input);
+    let mut p = parser::new(l);
 }
 
 #[test]
